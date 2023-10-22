e6e2c4af28b1a36e905f93e0e592f805de66b422
Ali George <george7ali@gmail.com>
2023-10-22 18:36:47 +0100

    a function to generate a fibonacci sequence of a number using recursion

--- fibonacci.js
+++ fibonacci.js
@@ -16,3 +16,16 @@ function fibs(num) {
 }
 let newArray = fibs(11);
 console.log(newArray);
+
+//fibonacci sequence by recursion
+function fibsRec(number) {
+    if(number == 2) {
+        return [0, 1]
+    } else {
+        let fibonacci = fibsRec(number - 1);
+        fibonacci.push(fibonacci[fibonacci.length - 1] + fibonacci[fibonacci.length - 2]);
+        return fibonacci;
+    }
+}
+let newFibsRec = fibsRec(10);
+console.log(newFibsRec);
